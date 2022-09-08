bd5bac5e819a4051e442ba9a72090b518ff2708d
valeriechinwe <v.c.k.a@outlook.com>
2022-09-08 12:22:57 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 
 /**
+ * main - Printing
  *
- * * main - Printing
+ * Return: Always 0 (Successful)
  *
- * *
- *
- * * Return: Always 0 (Successful)
- *
- * */
-
+ */
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual
-
-			puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
